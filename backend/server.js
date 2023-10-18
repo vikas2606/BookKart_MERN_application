9142448d41d5db9d2efbdedbfb91004fecee465a
vikas <vikasvasabathula@gmail.com>
2023-10-18 22:56:41 +0530

    Modified repo for deployment 1

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,8 @@ app.use("/api/cart",cartRoutes)
 const __dirname1=path.resolve()
 if(process.env.NODE_ENV==='production'){
 app.use(express.static(path.join(__dirname1,"/frontend/build")))
-app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"))
-})
+app.get('*',(req,res)=>
+    res.sendFile(path.resolve(__dirname1,"frontend","build","index.html")))
 }else{
     app.get("/",(req,res)=>{
         res.send("API is Running Successfully")
